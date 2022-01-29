19d020fd4a988ba1c69852bdcf8e81ca9534ce2f
Seung Ju <kingsky32@gmail.com>
2022-01-29 21:28:33 +0900

    ✅ Styles

--- front/src/components/layout/header/index.tsx
+++ front/src/components/layout/header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MenuButton from '#components/layout/header/menu_button';
-import GlobalNavigation from '#components/layout/header/globalNavigation';
+import GlobalNavigation from '#components/layout/header/global_navigation';
 
 class Header extends React.Component {
   render() {
