c407f112fa8509bcc8537869f25a1aa301926cda
Seung Ju <kingsky32@gmail.com>
2022-01-29 23:40:55 +0900

    ✅ Geting Mobile Portfolio Page

--- front/src/components/portfolio/list/item/index.tsx
+++ front/src/components/portfolio/list/item/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import gsap from 'gsap';
+import GsapScrollTrigger from 'gsap/ScrollTrigger';
 import Link from 'next/link';
 import moment from 'moment';
 import styles from './Item.module.scss';
@@ -11,13 +13,45 @@ export interface PortflioListItemProps
   htmlId?: string;
 }
 
+gsap.registerPlugin(GsapScrollTrigger);
+
 class PortfolioListItem extends React.Component<PortflioListItemProps> {
+  listItemRef = React.createRef<HTMLAnchorElement>();
+
+  componentDidMount() {
+    this.scrollTrigger();
+  }
+
+  componentWillUnmount() {
+    this.scrollTrigger();
+  }
+
+  scrollTrigger() {
+    console.log((this.listItemRef.current?.offsetTop ?? 0) - 25);
+    GsapScrollTrigger.create({
+      once: false,
+      trigger: this.listItemRef.current,
+      toggleClass: {
+        className: styles['active'],
+        targets: this.listItemRef.current,
+      },
+      start: (this.listItemRef.current?.offsetTop ?? 0) - window.innerHeight / 2 + 100,
+      end: this.listItemRef.current?.offsetTop ?? 0,
+    });
+  }
+
   render() {
     const { id, htmlId, title, subTitle, thumbnail, startAt, endAt, url, createdAt, updatedAt, ...props } = this.props;
 
     return (
       <Link href={url ?? `/portfolio/${id}`}>
-        <a id={htmlId} className={styles['wrapper']} style={{ backgroundImage: `url(${thumbnail.uri})` }} {...props}>
+        <a
+          ref={this.listItemRef}
+          id={htmlId}
+          className={classNames(styles['wrapper'])}
+          style={{ backgroundImage: `url(${thumbnail.uri})` }}
+          {...props}
+        >
           <div className={classNames(styles['container'])}>
             <b className={classNames(styles['title'])}>{title}</b>
             <p className={classNames(styles['contents'])}>
